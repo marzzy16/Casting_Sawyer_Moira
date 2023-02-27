d7264622bc505aeba5f7cd27e71f04f3029f07c9
marzzy16 <114106672+marzzy16@users.noreply.github.com>
2023-02-27 14:31:07 -0600

    Initial commit

---
+++ Casting_Sawyer_Moira/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Casting_Sawyer_Moira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asks user to enter a whole number and assigns input to integer
+            Console.WriteLine("Please enter a whole number!");
+            int myFaveNum = Convert.ToInt32(Console.ReadLine());
+
+            // Prints favorite number to console 
+            Console.WriteLine("Yourfavorite number is " + myFaveNum);
+
+            // Asks user a true/false question and assigns a response to a boolean
+            Console.WriteLine("Do you like video games?(Please enter 'true' or 'false'.");
+            bool isTrue = Convert.ToBoolean(Console.ReadLine());
+
+            // Print the result of question asked
+            Console.WriteLine($"It is {isTrue} that I like video games!");
+
+        }
+    }
+}
